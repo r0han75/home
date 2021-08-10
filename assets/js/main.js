07f471085db7efadbbec817ef498c75f3600e000
r0han75 <83898675+r0han75@users.noreply.github.com>
2021-08-10 10:24:15 +0545

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 !(function($) {
-  "use strict";
+  "use strict";fgdg
 
   // Smooth scroll for the navigation menu and links with .scrollto classes
   var scrolltoOffset = $('#header').outerHeight() - 1;
